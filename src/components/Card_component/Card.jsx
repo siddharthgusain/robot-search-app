2e691f7a4455914975b5fb75c2e229958c07bc80
siddharth gusain <siddharthgusain26011999@gmail.com>
2019-04-22 18:07:48 +0530

    added some styling

--- src/components/Card_component/Card.jsx
+++ src/components/Card_component/Card.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 
 
-const Card = () =>{
+const Card = ({ name,email,id }) =>{
     return (
-        <div>
-            <img alt="robots" src="https://robohash.org/lol" />
+        <div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
+            <img alt="robots" src={`https://robohash.org/${id}?200x200`} />
             <div>
-                <h2>Sid</h2>
-                <p>sid@gmail.com</p>
+                <h2>{name}</h2>
+                <p>{email}</p>
             </div>
         </div>
     );
--- src/index.js
+++ src/index.js
@@ -3,8 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Card from './components/Card_component/Card';
 import 'tachyons';
+import { robots } from './robot';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<Card />, document.getElementById('root'));
+ReactDOM.render(
+    <div>
+        <Card id={robots[0].id} name={robots[0].name} email={robots[0].email}/>
+        <Card id={robots[1].id} name={robots[1].name} email={robots[1].email}/>
+        <Card id={robots[2].id} name={robots[2].name} email={robots[2].email}/>
+        <Card id={robots[3].id} name={robots[3].name} email={robots[3].email}/>
+    </div>,
+    document.getElementById('root'));
 
 serviceWorker.unregister();
