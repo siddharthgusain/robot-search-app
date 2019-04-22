fe6cb17c5885c0e9ce5a8dbed1971d781fce7535
siddharth gusain <siddharthgusain26011999@gmail.com>
2019-04-22 18:17:41 +0530

    CardList compont

--- src/index.js
+++ src/index.js
@@ -1,18 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Card from './components/Card_component/Card';
+import CardList from './components/CardList_component/CardList';
 import 'tachyons';
 import { robots } from './robot';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-    <div>
-        <Card id={robots[0].id} name={robots[0].name} email={robots[0].email}/>
-        <Card id={robots[1].id} name={robots[1].name} email={robots[1].email}/>
-        <Card id={robots[2].id} name={robots[2].name} email={robots[2].email}/>
-        <Card id={robots[3].id} name={robots[3].name} email={robots[3].email}/>
-    </div>,
-    document.getElementById('root'));
+ReactDOM.render(<CardList robots={robots}/>,document.getElementById('root'));
 
 serviceWorker.unregister();
