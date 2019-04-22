96a7d0e4c98ce5e15f47cb67ab6596ca4e77eff8
siddharth gusain <siddharthgusain26011999@gmail.com>
2019-04-23 01:01:07 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore,applyMiddleware } from 'redux';
 import { searchRobots } from './reducers/reducer';
+import { createLogger } from 'redux-logger';
 import './index.css';
 import App from './containers/App';
 import 'tachyons';
 
 import * as serviceWorker from './serviceWorker';
-
-const store = createStore(searchRobots);
+const logger = createLogger();
+const store = createStore(searchRobots,applyMiddleware(logger));
 
 ReactDOM.render(
     <Provider store={ store }>
