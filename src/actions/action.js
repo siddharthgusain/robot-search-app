36d3c7869f9a5ff2ae04753faab0adf437806915
siddharth gusain <siddharthgusain26011999@gmail.com>
2019-04-23 00:02:58 +0530

    created reducer

--- src/actions/action.js
+++ src/actions/action.js
@@ -1,4 +1,6 @@
+import { CHANGE_SEARCH_FIELD } from '../constants/constant';
+
 export const setSearchField = (text) =>({
-    type:'CHANGE_SEARCH_FIELD',
+    type:CHANGE_SEARCH_FIELD,
     payload: text
 })  
