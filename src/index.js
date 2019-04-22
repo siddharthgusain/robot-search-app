aa79d8d1a04c9f73e848711149814e6f6b512d50
siddharth gusain <siddharthgusain26011999@gmail.com>
2019-04-23 00:48:56 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-dom';
+import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import { searchRobots } from './reducers/reducer';
 import './index.css';
