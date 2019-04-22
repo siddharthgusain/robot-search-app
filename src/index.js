22dd82b2de5638aab03c5ff41b5e1abdff8a6890
siddharth gusain <siddharthgusain26011999@gmail.com>
2019-04-23 00:08:17 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider,connect } from 'react-dom';
+import { createStore } from 'redux';
 import './index.css';
 import App from './containers/App';
 import 'tachyons';
 
 import * as serviceWorker from './serviceWorker';
+
 
 ReactDOM.render(<App />,document.getElementById('root'));
 
